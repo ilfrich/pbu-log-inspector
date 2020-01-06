ce11b608944cedc306a94ba759318f4155ca366b
Peter Ilfrich <peter.ilfrich@au1.ibm.com>
2020-01-07 10:40:10 +1100

    Added readme content to setup.py and increased version number for pip package

--- pip/setup.py
+++ pip/setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name="pbu-log-inspector",
-      version="0.0.1",
+      version="0.0.2",
       description="Flask endpoint delivering filtered log messages via REST",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url="https://github.com/ilfrich/pbu-log-inspector",
       author="Peter Ilfrich",
       author_email='peter.ilfrich@au1.ibm.com',
